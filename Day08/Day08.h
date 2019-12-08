0ba3df9d396ef6eae990a342746f118368076172
Viper Muffin <vipermuffin@gmail.com>
2019-12-08 09:58:08 -0500

    Initial Day 8 Solutions

---
+++ Day08/Day08.h
@@ -0,0 +1,16 @@
+//
+//  Advent of Code 2019 Solutions: Day 8
+//
+//  https://adventofcode.com/2019
+//
+//  Created by vipermuffin on 12/08/2019.
+//  Copyright © 2019 vipermuffin. All rights reserved.
+//
+#pragma once
+#include <string>
+#include <vector>
+
+namespace AocDay08 {
+//Function Definitions
+
+}
